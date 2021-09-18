1eec353360dbe866e842b7c0fa4b12415a88e02c
Aslihan Akbiyik <aslihana@yandex.com>
2021-09-18 19:53:46 +0300

    added files

---
+++ src/hashable.rs
@@ -0,0 +1,8 @@
+//think like its a interface
+pub trait Hashable {
+    fn bytes(&self) -> Vec<u8>;
+
+    fn hash(&self) -> Vec<u8> {
+        crypto_hash::digest(crypto_hash::Algorithm::SHA256, &self.bytes())
+    }
+}
